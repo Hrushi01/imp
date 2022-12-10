e4d6583274fca49759ed4ee395b2bcd1fe4daffb
Hrushi01 <hrushikesh.ambike@gmail.com>
2022-12-10 20:59:36 +0530

    All

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Login from "./login/Login";
+import Candidate from "./pages/Candidate";
+import { useState } from "react";
+import InterviewCam from "./components/InterviewCam";
+import { useRecordWebcam } from "react-record-webcam";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./components/InterviewCam.css";
+import MicroPhoneTesting from "./components/MicroPhoneTesting";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [Itr, setItr] = useState(true);
+
+  const recordWebcam = useRecordWebcam({ frameRate: 60 });
+  const saveFile = async () => {
+    const blob = await recordWebcam.getRecording();
+  };
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {show ? (
+        <div className="bg-gray-100 flex h-full w-full ">
+          <>
+            <div className="bg-gray-100 flex h-full w-full ">
+              <Navbar setItr={setItr} Itr={Itr} />
+
+              {Itr ? <InterviewCam /> : <Candidate />}
+            </div>
+          </>
+        </div>
+      ) : (
+        <Login show={show} setShow={setShow} />
+      )}
     </div>
   );
 }
